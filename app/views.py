546666344d1f536ee1d0fe590d201ae21093d38e
georis <georiseno@gmail.com>
2023-08-10 16:11:18 +0100

    nee

--- app/views.py
+++ app/views.py
@@ -326,7 +326,7 @@ def post_post(request):
             'reason' : "La somme restante pour ce post ne peut que vous payez pour un nombre de vue inférieure à " + str(int((post.total_invest - post.already_used)/my_story.price() * 10)) + "."
         })
     accepted = AcceptedPost.objects.create(
-        post=post, story=my_story, goals=goal, payc=int(int(GeoxDetails.objects.get(key ='price:10:' + post.get_media_typ()).value) * goal / 10))
+        post=post, story=my_story, goals=goal, payc=int(int(GeoxDetails.objects.get(key ='price:10:' + post.get_media_typ() + ':real').value) * goal / 10))
     my_story.proposed_posts.remove(post)
     my_story.save()
     return Response({
